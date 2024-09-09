69c551281a0b03ed4e6cf457f0aa9349853fa046
Matias Bazzano <matiasbazzano19@gmail.com>
2024-09-08 23:30:45 -0300

    added newsletters action

--- js/newsletters.js
+++ js/newsletters.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const popupOverlay = document.getElementById('subscribePopup');
     const popupContent = document.querySelector('.popup-content');
     const closePopupBtn = document.getElementById('closePopup');
+    const subscribeForm = document.getElementById('subscribeForm');
 
     setTimeout(() => {
         popupOverlay.style.display = 'flex';
@@ -25,5 +26,31 @@ document.addEventListener('DOMContentLoaded', () => {
                 popupOverlay.style.display = 'none';
             }, 500);
         }
+    });
+
+    subscribeForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const phone = 'N/A';
+        const message = 'N/A';
+
+        emailjs.send('KNAK_WEBSITE_SERVICE', 'ADMIN_TEMPLATE_ID', {
+            from_name: name,
+            from_email: email,
+            phone: phone, 
+            message: message,
+            to_email: 'netwave.contact@gmail.com'
+        }).then(() => {
+            alert('Gracias por suscribirte.');
+            event.target.reset();
+            popupContent.classList.remove('show');
+            setTimeout(() => {
+                popupOverlay.style.display = 'none';
+            }, 500);
+        }).catch(error => {
+            console.error('Error al enviar el correo:', error);
+        });
     });
 });
